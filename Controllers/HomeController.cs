51ca7e45bccc085119d7d21654352f6826ec2959
CARLOS MALDONADO <karloz_0986@hotmail.com>
2021-12-27 13:01:21 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using ceaweb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ceaweb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MySqlDatabase _database;
+
+        public HomeController(ILogger<HomeController> logger, MySqlDatabase mySqlDatabase)
+        {
+            _logger = logger;
+            _database = mySqlDatabase;
+         
+        }
+
+        public IActionResult Index()
+        {
+            var cmd = _database.Connection.CreateCommand();
+            cmd.CommandText = "Select * from usuarios";
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0));
+                    }
+                }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MySqlDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ceaweb
+{
+    public class MySqlDatabase : IDisposable
+    {
+       public MySqlConnection Connection { get; set; }
+
+        public MySqlDatabase(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+            Connection.Open();
+        }
+        public void Dispose()
+        {
+            Connection.Close();
+        }
+    }
+}
